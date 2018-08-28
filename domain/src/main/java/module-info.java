8fe6ea19668d157a24d92415e7c28c484476b426
Ronan Le Roy <rleroy@effisoft-group.com>
2018-08-28 11:48:51 +0200

    Add modules.

---
+++ domain/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module com.effisoft.kata.calculator.domain {
+    requires log4j;
+    exports com.effisoft.kata.calculator.domain.shell;
+}
---
+++ infra/src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module com.effisoft.kata.calculator.infra {
+    requires com.effisoft.kata.calculator.domain;
+    requires log4j;
+    requires java.xml.bind;
+}
